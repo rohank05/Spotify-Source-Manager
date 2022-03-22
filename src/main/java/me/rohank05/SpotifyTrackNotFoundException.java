7d507d5bbf4fd25919d48242e3a37915fd43716d
rohan <rohan.shuvam@gmail.com>
2022-03-22 15:14:03 +0530

    Some changes to playlist

--- src/main/java/me/rohank05/SpotifyTrackNotFoundException.java
+++ src/main/java/me/rohank05/SpotifyTrackNotFoundException.java
@@ -1,8 +1,8 @@
 package me.rohank05;
 
-import java.io.Serial;
+
 
 public class SpotifyTrackNotFoundException extends RuntimeException{
-    @Serial
+
     private static final long serialVersionUID = 6550093849278285754L;
 }
